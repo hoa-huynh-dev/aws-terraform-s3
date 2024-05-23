3e333fb3a064316a66df1716ecc874fcc6b7b98e
Hoà Huỳnh <huynhbahoa1998@gmail.com>
2024-05-23 15:56:11 +0700

    Add terraform module for S3 bucket

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -5,5 +5,5 @@ locals {
 resource "aws_s3_bucket" "bucket" {
   bucket = local.bucket_name
 
-  tags = merge(var.bucket_tags, { "name" : var.bucket_name })
+  tags = merge(var.bucket_tags, { "name" : local.bucket_name })
 }
