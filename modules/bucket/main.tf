1bbd506badec53224b23a20480a5b4182bf9e087
Hoà Huỳnh <huynhbahoa1998@gmail.com>
2024-05-23 15:46:30 +0700

    Add terraform module for S3 bucket

--- modules/bucket/main.tf
+++ modules/bucket/main.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
+  bucket        = var.bucket_name
+  bucket_prefix = var.bucket_prefix
 
   tags = merge(var.bucket_tags, { "name" : var.bucket_name })
 }
--- modules/bucket/variables.tf
+++ modules/bucket/variables.tf
@@ -2,7 +2,11 @@ variable "bucket_name" {
   type = string
 }
 
+variable "bucket_prefix" {
+  type = string
+}
+
 variable "bucket_tags" {
-  type = map(string)
+  type    = map(string)
   default = {}
 }
